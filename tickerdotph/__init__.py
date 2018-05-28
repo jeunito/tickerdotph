b4bb8d1c28a0559405b8d253325587e7846a04e8
Jose Asuncion <jeunito@gmail.com>
2018-05-28 14:00:44 -0700

    convert to rest api

--- tickerdotph/__init__.py
+++ tickerdotph/__init__.py
@@ -1,10 +1,16 @@
 from tickerdotph.institutions import bpi
 import json
 
-def main(*args):
+from bottle import get, run
+
+@get('/ping')
+def ping(*args):
     funds = bpi.funds()
     if len(args) == 0:
         return funds
     elif len(args) == 2:
         fund_name = args[0].get("name", None)
         return dict(name=fund_name, price=funds.get(fund_name))
+
+def main():
+    run(host="0.0.0.0", port=8080, debug=True)
