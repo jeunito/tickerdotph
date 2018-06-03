e5396df45b75ea6739868a3017435a7bb0a88a86
Jose Asuncion <jeunito@gmail.com>
2018-06-03 15:33:08 -0700

    add api for fund

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import webapp2
 from tickerdotph.institutions import bpi
 import json
 
-class MainPage(webapp2.RequestHandler):
+class FundsController(webapp2.RequestHandler):
     def get(self):
         funds = bpi.funds()
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(funds))
 
+class FundController(webapp2.RequestHandler):
+    def get(self, fund_ticker):
+        funds = bpi.funds()
+        fund_price = funds.get(fund_ticker, None)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps({ fund_ticker : fund_price }))
+
+
 app = webapp2.WSGIApplication([
-    ('/health', MainPage)
+    ('/funds', FundsController),
+    ('/funds/([A-Za-z]+)', FundController)
 ], debug=True)
