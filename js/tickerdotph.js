2b13c41a1e06f95a2e4a66c33bfd1c48c36aa2f2
Jose Asuncion <jeunito@gmail.com>
2018-06-05 21:21:48 -0700

    change url path for a fund

--- js/tickerdotph.js
+++ js/tickerdotph.js
@@ -1,5 +1,5 @@
 function tickerdotph(ticker) {
-    var url = "${URL}/fund?name=" + ticker; // replace ${URL} variable with url to tickerdotph rest api
+    var url = "${URL}/funds/" + ticker; // replace ${URL} variable with url to tickerdotph rest api
     var res = UrlFetchApp.fetch(url).getContentText();
     var fund = JSON.parse(res);
     return fund.price
